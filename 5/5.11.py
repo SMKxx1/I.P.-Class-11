bae172aa72e5231753214870e52fd259438f3e18
Shahad Mustafa Khan <Shahad.mustafa003@gmail.com>
2019-02-20 18:53:06 +0530

    Create 5.11.py

---
+++ 5/5.11.py
@@ -0,0 +1,15 @@
+n = int(input("Enter a number > 20: "))
+
+
+if n > 20:
+    for i in range(11,n+1):
+        if i % 3 == 0 and i % 7 == 0:
+            print(i,'TipsyTopsy')
+        elif i % 3 == 0:
+            print(i,'Tipsy')
+        elif i % 7 == 0:
+            print(i,'Topsy')
+        else:
+            print(i)
+else:
+    print("Number is smaller then or equal to 20")
