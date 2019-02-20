e648cdb90fc6d3f547393b8db0e5650c76ee7294
Shahad Mustafa Khan <Shahad.mustafa003@gmail.com>
2019-02-20 15:44:02 +0530

    Create 5.6.py

---
+++ 5.6.py
@@ -0,0 +1,8 @@
+words = {1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine',0:'zero'}
+
+a = int(input("Enter a number between 0 and 9: "))
+
+if a < 0 or a > 9:
+    print("Invalid")
+else:
+    print(words[a])
