9261b2ba8166d2e5520fd070f97beba901df4907
Shahad Mustafa Khan <Shahad.mustafa003@gmail.com>
2019-02-20 15:01:41 +0530

    ...

---
+++ 5.1.py
@@ -0,0 +1,11 @@
+def con():
+    z = int(input("Enter the length in CM: "))
+    if z >= 0:
+        print(z/2.54,"Is the length in inches")
+    else:
+        print("Invalid")
+        input()
+        con()
+con()
+
+
