4af9bc08286ecf99696b10cf36470ab10ffc63e0
Shahad Mustafa Khan <Shahad.mustafa003@gmail.com>
2019-02-20 18:47:34 +0530

    Create 5.9.py

---
+++ 5/5.9.py
@@ -0,0 +1,15 @@
+List = []
+count = int(input("Enter the length of the list: "))
+
+for i in range(count):
+    num = int(input(">>> "))
+    List.append(num)
+
+Sum = 0
+
+for i in List:
+    Sum += i
+
+avg = Sum/len(List)
+
+print("The average is",avg)
