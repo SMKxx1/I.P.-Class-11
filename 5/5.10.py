96145422efe36840132e342b26f51c43b09eef89
Shahad Mustafa Khan <Shahad.mustafa003@gmail.com>
2019-02-20 16:24:44 +0530

    ......

---
+++ 5/5.10.py
@@ -0,0 +1,12 @@
+a = int(input("Enter a number: "))
+
+if a % 10 == 4:
+    print("It ends with a 4")
+elif a % 10 == 8:
+    print("It ends with a 8")
+elif a == 4:
+    print("Its 4")
+elif a == 8:
+    print("Its 8")
+else:
+    print("Ends with neither")
