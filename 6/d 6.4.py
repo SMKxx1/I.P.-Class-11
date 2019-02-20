bb982174f719e79020751c71e51777e39d30495c
Shahad Mustafa Khan <Shahad.mustafa003@gmail.com>
2019-02-20 22:09:47 +0530

    ...

--- 6/d 6.4.py
+++ 6/d 6.4.py
@@ -4,4 +4,12 @@ while True:
         break
     else:
         pass
-    print(s)
+    for i in s:
+        if i.islower():
+            print(i.upper(),end="")
+        elif i.isupper():
+            print(i.lower(),end="")
+        else:
+            print(" ",end="")
+    print()
+    
